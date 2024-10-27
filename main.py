53f8aef27f5102b560b7537f70d93981812eabca
Lucas <idamasceno@apoioprodesp.sp.gov.br>
2024-10-27 19:05:40 -0300

    feat: adicionando efeitos sonoros

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ import pandas as pd
 import os
 #import openpyxl
 
+from pygame import mixer
+
 # Inicializa o Pygame
 pygame.init()
+mixer.init()
+
+
+# Configurações mixer
+mixer.music.set_volume(0.2)
 
 # Configurações da tela
 WIDTH, HEIGHT = 800, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Jeremias,The Genius')
+pygame.display.set_caption('Jeremias The Genius')
 
 # Cores
 COLORS = {
@@ -79,21 +86,23 @@ def draw_buttons(highlight=None):
     
     for direction in COLORS.keys():
         color = LIGHT_COLORS[direction] if direction == highlight else COLORS[direction]
-        if direction == 'UP':
+        if direction == 'UP':          
             pygame.draw.rect(screen, color, (150, 50, 100, 100))
-        elif direction == 'DOWN':
+        elif direction == 'DOWN':    
             pygame.draw.rect(screen, color, (150, 250, 100, 100))
-        elif direction == 'LEFT':
+        elif direction == 'LEFT':          
             pygame.draw.rect(screen, color, (50, 150, 100, 100))
         elif direction == 'RIGHT':
-            pygame.draw.rect(screen, color, (250, 150, 100, 100))
-            
+            pygame.draw.rect(screen, color, (250, 150, 100, 100))    
     pygame.display.flip()
 
 # Função para mostrar a sequência para o jogador
 def show_sequence():
     for step in sequence:
         draw_buttons(step)
+        sound_file = f"assets/sounds/{step.lower()}.wav" 
+        mixer.music.load(sound_file) 
+        mixer.music.play()  
         pygame.time.delay(step_duration)
         draw_buttons()
         pygame.time.delay(200)
@@ -116,6 +125,9 @@ def reset_highlight():
 
 # Função para iniciar o jogo
 def start_game():
+    mixer.music.load("assets\sounds\menu-selected.wav")
+    mixer.music.play()
+
     global user_sequence, score, game_over
     user_sequence = []
     score = 0
@@ -141,18 +153,26 @@ def game_loop():
             if not game_over:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
+                        mixer.music.load("assets/sounds/up.wav")
+                        mixer.music.play()
                         user_sequence.append('UP')
                         highlighted_button = 'UP'
                         draw_buttons('UP')
                     elif event.key == pygame.K_DOWN:
+                        mixer.music.load("assets/sounds/down.wav")
+                        mixer.music.play()
                         user_sequence.append('DOWN')
                         highlighted_button = 'DOWN'
                         draw_buttons('DOWN')
                     elif event.key == pygame.K_LEFT:
+                        mixer.music.load("assets/sounds/left.wav")
+                        mixer.music.play()
                         user_sequence.append('LEFT')
                         highlighted_button = 'LEFT'
                         draw_buttons('LEFT')
                     elif event.key == pygame.K_RIGHT:
+                        mixer.music.load("assets/sounds/right.wav")
+                        mixer.music.play()
                         user_sequence.append('RIGHT')
                         highlighted_button = 'RIGHT'
                         draw_buttons('RIGHT')
@@ -189,6 +209,9 @@ def update_ranking(new_score):
 
 # Função para exibir a tela de Game Over
 def display_game_over():
+    mixer.music.load("assets/sounds/error.wav")
+    mixer.music.play()
+
     screen.fill((255, 0, 0))
     font = pygame.font.Font(None, 74)
     text_game_over = font.render('Game Over', True, (255, 255, 255))
@@ -204,6 +227,9 @@ def exit_game():
 
 # Função para mostrar os créditos
 def show_credits():
+    mixer.music.load("assets\sounds\menu-selected.wav")
+    mixer.music.play()
+
     credits = pygame_menu.Menu('Créditos', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
     credits.add.label('Alunos:')
     credits.add.label('Lucas Damasceno da Cunha Lima')
@@ -215,6 +241,9 @@ def show_credits():
 
 # Função para criar o menu de entrada do nome do jogador
 def enter_name_menu():
+    mixer.music.load("assets\sounds\menu-selected.wav")
+    mixer.music.play()
+
     global player_name
     name_menu = pygame_menu.Menu('Digite seu Nome', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
     name_menu.add.text_input('Nome: ', default=player_name, onreturn=lambda name: set_player_name(name))
@@ -229,11 +258,11 @@ def set_player_name(name):
 
 # Função para criar o menu
 def show_menu():
-    menu = pygame_menu.Menu('Genius Game', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
+    menu = pygame_menu.Menu('Jeremias The Genius', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
 
-    menu.add.button('Começar Jogo', start_game)  # Opção para iniciar o jogo
-    menu.add.button('Alterar Nome', enter_name_menu)  # Opção para alterar nome
-    menu.add.button('Ranking', show_ranking)  # Modificado para "Ranking"
+    menu.add.button('Novo Jogo', start_game)
+    menu.add.button('Alterar Apelido', enter_name_menu)
+    menu.add.button('Ranking', show_ranking) 
     menu.add.button('Créditos', show_credits)
     menu.add.button('Sair', exit_game)
 
@@ -242,7 +271,7 @@ def show_menu():
 # Função para mostrar o ranking
 def show_ranking():
     ranking_menu = pygame_menu.Menu('Ranking', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
-    ranking_menu.add.label('Ranking dos Melhores Resultados:')
+    ranking_menu.add.label('Melhores Resultados:')
     
     # Adiciona os 10 melhores resultados ao menu
     for idx, (score, name) in enumerate(ranking):
