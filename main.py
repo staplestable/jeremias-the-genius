9a5b53cbe80438dd8b927fc9bd22f54733b8bfce
Lucas Iudi Corregliano Gallinari <83624858+Lucas-IudiCG@users.noreply.github.com>
2024-11-04 19:15:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,12 @@ WIDTH, HEIGHT = 1000, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Jeremias, The Genius')
 first_game=True
-
+Fase_unlimited = 1
+lives = 3
+menu_lives = lives
+life_image_path = 'assets/Vida.png'  # Coloque o caminho para sua imagem de vida
+life_image = pygame.image.load(life_image_path)
+life_image = pygame.transform.scale(life_image, (70, 70))
 # Carrega a imagem de fundo e redimensiona para caber na tela
 background_image_path = 'assets/Tela_jogo.png'  # Coloque o caminho para sua imagem de fundo
 background_image = pygame.image.load(background_image_path)
@@ -59,10 +64,11 @@ time_remaining = time_limit
 # Variável para controlar o estado da demonstração
 showing_sequence = False
 
-def set_time_limit(limit, settings):
+def set_time_limit(limit, lives, settings):
     global time_limit
     mixer.music.load("assets/sounds/menu-selected.wav")
     mixer.music.play()
+    set_lives(lives)
     time_limit = limit
     reset_timer()
 
@@ -72,23 +78,22 @@ def reset_timer():
 
 def reset_timer1():
     global time_remaining
-    time_remaining = time_limit + len(sequence)+1  # Tempo restante baseado no número de passos
+    time_remaining = time_limit + len(sequence)  # Tempo restante baseado no número de passos
 
 # Função para carregar o ranking do arquivo Excel
+def save_ranking():
+    with open(ranking_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows([(score, name, fase) for score, name, fase in ranking]) 
+
 def load_ranking():
     global ranking
     if os.path.exists(ranking_file):
         with open(ranking_file, mode='r') as file:
             reader = csv.reader(file)
-            ranking = [(int(score), name) for score, name in reader]
+            ranking = [(int(score), name, int(fase)) for score, name, fase in reader]
     else:
         ranking = []
-
-# Função para salvar o ranking em um arquivo Excel
-def save_ranking():
-    with open(ranking_file, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(ranking) 
 
 # Função para adicionar um novo passo à sequência
 def add_step():
@@ -107,11 +112,14 @@ def dim_screen():
 
 # Função para desenhar os botões e o timer
 def draw_game_elements(highlight=None):
+    global Fase_unlimited, lives, score  # Adicione 'lives' aqui para exibir na tela
+
     # Define a imagem de fundo
     screen.blit(background_image, (0, 0))  # Aplica a imagem como plano de fundo
-    var_height = HEIGHT // 3+ 50
+    var_height = HEIGHT // 3 + 50
     var_width = WIDTH // 6
     diamond_size = 50
+
     # Define os vértices dos diamantes
     diamonds = {
         'UP': [(var_height + 150, var_width + 50), (var_height + 150 + diamond_size, var_width + 50 + diamond_size), (var_height + 150, var_width + 50 + 2 * diamond_size), (var_height + 150 - diamond_size, var_width + 50 + diamond_size)],
@@ -119,15 +127,20 @@ def draw_game_elements(highlight=None):
         'LEFT': [(var_height + 100, var_width + 100), (var_height + 100 + diamond_size, var_width + 100 + diamond_size), (var_height + 100, var_width + 100 + 2 * diamond_size), (var_height + 100 - diamond_size, var_width + 100 + diamond_size)],
         'RIGHT': [(var_height + 200, var_width + 200), (var_height + 200 + diamond_size, var_width + 200 - diamond_size), (var_height + 200, var_width + 200 - 2 * diamond_size), (var_height + 200 - diamond_size, var_width + 200 - diamond_size)]
     }
+
     # Desenha os botões
     for direction in COLORS.keys():
         color = LIGHT_COLORS[direction] if direction == highlight else COLORS[direction]
         pygame.draw.polygon(screen, color, diamonds[direction])
 
-    # Desenha o timer
+    # Desenha o timer e a fase
     font = pygame.font.Font(None, 36)
-    timer_surface = font.render(f'Tempo: {time_remaining}', True, (255, 255, 255))
-    screen.blit(timer_surface, (WIDTH//3 , 100))  # Posição do timer
+    info_surface = font.render(f'Tempo: {time_remaining}   Fase: {Fase_unlimited}   Score: {score}', True, (255, 255, 255))
+    screen.blit(info_surface, (WIDTH // 4, 100))  # Posição das informações
+
+    # Desenha o sprite de vida para cada vida restante
+    for i in range(lives):
+        screen.blit(life_image,( 75 + i * 35, 50))  # Ajusta o espaçamento entre cada imagem
 
     pygame.display.flip()  # Atualiza a tela
 
@@ -148,13 +161,18 @@ def show_sequence():
 
 # Função para verificar a sequência do usuário
 def check_sequence():
-    global game_over
-    if user_sequence == sequence:
-        return True
-    elif len(user_sequence) >= len(sequence):
-        game_over = True
-        return False
-    return None
+    global game_over, lives
+    for i in range(len(user_sequence)):
+        if user_sequence[i] != sequence[i]:
+            lives -= 1  # Perde uma vida ao errar a sequência
+            mixer.music.load("assets/sounds/error.wav")
+            mixer.music.play()
+            if lives <= 0:
+                game_over = True  # Termina o jogo se as vidas acabarem
+                return False
+            else:
+                return True 
+    return True if len(user_sequence) == len(sequence) else None
 
 # Função para retornar a cor do botão pressionado
 def reset_highlight():
@@ -163,20 +181,20 @@ def reset_highlight():
     draw_game_elements()  # Redesenha sem destaque
 
 # Função para iniciar o jogo
-def start_game():
+def start_game_unlimited():
     mixer.music.load("assets/sounds/menu-selected.wav")
     mixer.music.play()
-    global user_sequence, score, game_over
+    global user_sequence, score, game_over, lives
     user_sequence = []
     score = 0
     game_over = False
+    lives = menu_lives  # Usa o valor de vidas ajustado no menu
     sequence.clear()
 
-    # Timer de 3 segundos antes de começar o jogo
     countdown_timer(3)
+    add_step()
+    game_loop_unlimited()
 
-    add_step()  # Inicia o jogo após o atraso
-    game_loop()
 
 # Função para exibir a contagem regressiva
 def countdown_timer(seconds):
@@ -205,9 +223,9 @@ def highlight_button(direction):
     reset_highlight()  # Limpa o destaque após o delay
 
 # Loop principal do jogo
-def game_loop():
-    global user_sequence, score, game_over, highlighted_button, ranking, time_remaining, first_game
-
+def game_loop_unlimited():
+    global user_sequence, score, game_over, highlighted_button, ranking, time_remaining, first_game, Fase_unlimited
+    Fase_unlimited = 1 
     # Inicializa o tempo restante
     if first_game:
         reset_timer()
@@ -249,16 +267,18 @@ def game_loop():
 
                     reset_timer()  # Reinicia o tempo ao pressionar um botão
                     result = check_sequence()
+                    if len(sequence)>=5*Fase_unlimited:
+                        Fase_unlimited+=1
                     if result is True:
                         dim_screen()
                         pygame.time.delay(500)
-                        score += 1 + int((time_sum * len(sequence))/time_limit)
+                        score += 1 + int((time_sum * len(sequence))/(time_limit*lives))*Fase_unlimited
                         user_sequence = []
                         add_step()
                         time_sum = 0
                     elif result is False:
                         game_over = True
-                        update_ranking(score)
+                        update_ranking(score, Fase_unlimited)
 
                 if event.type == timer_event and not showing_sequence:
                     time_remaining -= 1
@@ -277,12 +297,27 @@ def game_loop():
         pygame.time.delay(50)
 
 # Função para atualizar o ranking
-def update_ranking(new_score):
+def update_ranking(new_score, fase_unlimited):
     global ranking
-    ranking.append((new_score, player_name))
+    ranking.append((new_score, player_name, fase_unlimited))
     ranking = sorted(ranking, key=lambda x: x[0], reverse=True)[:10]  # Mantém os 10 melhores
     save_ranking()  # Salva o ranking atualizado
+    
+def difficulty_menu():
+    # Criação do submenu de dificuldade
+    settings = pygame_menu.Menu('Configurações de Dificuldade', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
+    
+    # Entrada de texto para o nome do jogador
+    settings.add.text_input('Nome do Jogador: ', default=player_name, onchange=set_player_name)
 
+    # Botões para ajustar a dificuldade, que afeta o tempo do timer
+    settings.add.button('Fácil', lambda: set_time_limit(10, 5, settings))
+    settings.add.button('Médio', lambda: set_time_limit(7, 3, settings))
+    settings.add.button('Difícil', lambda: set_time_limit(5, 2, settings))
+    settings.add.button('Jeremias', lambda: set_time_limit(3, 1, settings))
+    settings.add.button('Voltar', show_menu)  # Botão para voltar ao menu principal
+    
+    settings.mainloop(screen)
 # Função para exibir a tela de Game Over
 def display_game_over():
     # Exibe a imagem de fundo
@@ -292,8 +327,8 @@ def display_game_over():
     screen.blit(tela_image, (0, 0))
     
     # Toca o som de erro
-    mixer.music.load("assets/sounds/error.wav")
-    mixer.music.play()
+    #mixer.music.load("assets/sounds/error.wav")
+    #mixer.music.play()
     
     # Configura a fonte e renderiza o texto de "Game Over" e "Score"
     font = pygame.font.Font(None, 74)
@@ -338,63 +373,59 @@ def settings_menu():
     screen.blit(background_image, (0, 0))  # Aplica a imagem de fundo nas configurações
     mixer.music.load("assets/sounds/menu-selected.wav")
     mixer.music.play()
-    global time_limit
+    global time_limit, lives, player_name  # Adicione a variável de vidas aqui para controle no menu
+    
+    # Inicializa o menu de configurações com tema escuro
     settings = pygame_menu.Menu('Configurações', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
     
-    # Adiciona entrada de texto para o nome do jogador
-    settings.add.text_input('Nome do Jogador: ', default=player_name, onreturn=lambda name: set_player_name(name))
-    
-    # Adiciona botões para dificuldade
-    settings.add.button('Fácil', lambda: set_time_limit(10, settings))
-    settings.add.button('Médio', lambda: set_time_limit(7, settings))
-    settings.add.button('Difícil', lambda: set_time_limit(5, settings))
-    settings.add.button('Jeremias', lambda: set_time_limit(3, settings))
-    
-    # Adiciona o range_slider para o tempo do timer
-    default_value = time_limit  # Usa o valor atual do timer
-    range_values = (1, 10)  
-    increment = 1  # Define o incremento do slider
+    # Entrada de texto para o nome do jogador
+    settings.add.text_input('Nome do Jogador: ', default=player_name, onchange=set_player_name)
+    # Slider para ajustar o tempo do timer em segundos
     settings.add.range_slider('Tempo do Timer (segundos):', 
-                              default=default_value, 
-                              range_values=range_values, 
-                              increment=increment,
-                              onchange=lambda value: (set_time_limit(value, settings), reset_timer()))
-                              
-    settings.add.range_slider('Tempo do Timer (segundos):', 
-                              default=default_value, 
-                              range_values=range_values, 
-                              increment=increment,
-                              onchange=lambda value: (set_time_limit(value, settings), reset_timer()))  # Atualiza o timer em tempo real
+                              default=time_limit, 
+                              range_values=(1, 10), 
+                              increment=1,
+                              onchange=lambda value: set_timer(value))  # Atualiza o tempo do timer
     
-    # Adiciona o range_slider para o volume da música
-    volume_default = 0.2  # Valor inicial do volume
-    volume_range = (0.0, 1.0)
-    
+    # Slider para ajustar o volume da música
     settings.add.range_slider('Volume da Música:', 
-                              default=volume_default, 
-                              range_values=volume_range, 
+                              default=mixer.music.get_volume(),  # Pega o volume atual
+                              range_values=(0.0, 1.0), 
                               increment=0.1,
-                              onchange=lambda volume: mixer.music.set_volume(volume))  # Atualiza o volume em tempo real
+                              onchange=lambda volume: mixer.music.set_volume(volume))  # Atualiza o volume
     
-    # Botões de voltar
+    # Slider para ajustar o número de vidas do jogador
+    settings.add.range_slider('Número de Vidas:', 
+                              default=lives, 
+                              range_values=(1, 5), 
+                              increment=1,
+                              onchange=lambda value: set_lives(value))  # Atualiza o número de vidas
+    
+    # Botão para voltar ao menu principal
     settings.add.button('Voltar', show_menu)
     
+    # Loop para exibir e atualizar o menu
     while True:
         events = pygame.event.get()  # Obtém os eventos do Pygame
         settings.update(events)  # Atualiza o menu com os eventos
         settings.draw(screen)  # Desenha o menu na tela
-        pygame.display.flip()
+        pygame.display.flip()    # Atualiza a tela
 
+def set_lives(value):
+    global lives, menu_lives
+    lives = int(value)  # Atualiza a variável global de vidas
+    menu_lives = lives
 # Função para definir o tempo do timer
 def show_menu():
     screen.blit(background_image, (0, 0))  # Aplica a imagem de fundo no menu principal
     menu = pygame_menu.Menu('Genius Game', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
 
-    menu.add.button('Começar Jogo', start_game)  # Opção para iniciar o jogo
+    menu.add.button('Começar Jogo', start_game_unlimited)  # Opção para iniciar o jogo
+    menu.add.button('Dificuldade', difficulty_menu)  # Opção para o menu de dificuldades
     menu.add.button('Configurações', settings_menu)  # Opção para configurações
     menu.add.button('Ranking', show_ranking)  # Modificado para "Ranking"
     menu.add.button('Créditos', show_credits)
-    menu.add.button('Sair', exit_game)
+    menu.add.button('Sair', exit_game) 
 
     menu.mainloop(screen)
 def set_timer(value):
@@ -408,9 +439,9 @@ def show_ranking():
     ranking_menu = pygame_menu.Menu('Ranking', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
     ranking_menu.add.label('Ranking dos Melhores Resultados:')
     
-    # Adiciona os 10 melhores resultados ao menu
-    for idx, (score, name) in enumerate(ranking):
-        ranking_menu.add.label(f'{idx + 1}. {name}: {score}')
+    # Adiciona os 10 melhores resultados ao menu com fase incluída
+    for idx, (score, name, fase) in enumerate(ranking):
+        ranking_menu.add.label(f'{idx + 1}. {name}: {score} (Fase {fase})')
     
     ranking_menu.add.button('Voltar', show_menu)
     ranking_menu.mainloop(screen)
