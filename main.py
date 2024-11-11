6d2adbbfda9cd7976bf657e9424ef5e99460a4e1
Lucas Iudi Corregliano Gallinari <83624858+Lucas-IudiCG@users.noreply.github.com>
2024-11-11 19:02:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ pygame.init()
 mixer.init()
 
 # Configurações da tela
-BACKGROUND_COLOR = (51, 23, 51) 
+BACKGROUND_COLOR = (51, 23, 51)
 mixer.music.set_volume(0.2)
 WIDTH, HEIGHT = 1000, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Jeremias, The Genius')
-first_game=True
+first_game = True
 Fase_unlimited = 1
 lives = 3
 menu_lives = lives
@@ -64,10 +64,28 @@ time_remaining = time_limit
 # Variável para controlar o estado da demonstração
 showing_sequence = False
 
+# Variável de controle de pausa
+game_paused = False
+def restore_colors():
+    global COLORS, LIGHT_COLORS
+    COLORS = {
+        'UP': (255, 0, 0),
+        'DOWN': (0, 255, 0),
+        'LEFT': (0, 0, 255),
+        'RIGHT': (255, 255, 0),
+    }
+
+    LIGHT_COLORS = {
+        'UP': (255, 128, 128),
+        'DOWN': (128, 255, 128),
+        'LEFT': (128, 128, 255),
+        'RIGHT': (255, 255, 128),
+    }
 def play_theme_music():
     try:
+        # Carrega a música de fundo
         mixer.music.load("assets/sounds/game_music.mp3")  # Substitua pelo caminho correto do seu arquivo de música
-        mixer.music.play(-1, 0.0)  # O -1 faz com que a música toque em loop, e 0.0 é o tempo de atraso
+        mixer.music.play(-1, 0.0)  # O -1 faz com que a música toque em loop
     except pygame.error as e:
         print(f"Erro ao carregar a música: {e}")
 
@@ -79,6 +97,19 @@ def shuffle_colors():
     shuffled_light_colors = {keys[i]: LIGHT_COLORS[original] for i, original in enumerate(LIGHT_COLORS.keys())}
     
     return shuffled_colors, shuffled_light_colors
+
+# Função para adicionar um novo passo à sequência
+def add_step():
+    global COLORS, LIGHT_COLORS
+    step = random.choice(list(COLORS.keys()))
+    sequence.append(step)
+    show_sequence()
+
+    # Embaralha as cores após a fase 3
+    if Fase_unlimited >2 and Fase_unlimited!=3:
+        COLORS, LIGHT_COLORS = shuffle_colors()
+        
+
 def set_time_limit(limit, lives, settings):
     global time_limit
     mixer.music.load("assets/sounds/menu-selected.wav")
@@ -86,7 +117,11 @@ def set_time_limit(limit, lives, settings):
     set_lives(lives)
     time_limit = limit
     reset_timer()
-
+    
+def set_fase(value):
+    global Fase_unlimited
+    Fase_unlimited = int(value)
+    
 def reset_timer():
     global time_remaining
     time_remaining = time_limit
@@ -111,15 +146,7 @@ def load_ranking():
         ranking = []
 
 # Função para adicionar um novo passo à sequência
-def add_step():
-    global COLORS, LIGHT_COLORS
-    step = random.choice(list(COLORS.keys()))
-    sequence.append(step)
-    show_sequence()
 
-    # Embaralha as cores após a fase 3
-    if Fase_unlimited >=3:
-        COLORS, LIGHT_COLORS = shuffle_colors()
 # Função para escurecer a tela
 def dim_screen():
     overlay = pygame.Surface((WIDTH, HEIGHT))
@@ -133,10 +160,16 @@ def dim_screen():
 def draw_game_elements(highlight=None):
     global Fase_unlimited, lives, score  # Adicione 'lives' aqui para exibir na tela
 
+    # Verifica a condição de fase para definir a largura e altura dos botões
+    if Fase_unlimited >= 4:
+        var_width = random.randint(100, 300)
+        var_height = random.randint(100, 300)
+    else:
+        var_height = HEIGHT // 3 + 50
+        var_width = WIDTH // 6
+
     # Define a imagem de fundo
     screen.blit(background_image, (0, 0))  # Aplica a imagem como plano de fundo
-    var_height = HEIGHT // 3 + 50
-    var_width = WIDTH // 6
     diamond_size = 50
 
     # Define os vértices dos diamantes
@@ -159,7 +192,7 @@ def draw_game_elements(highlight=None):
 
     # Desenha o sprite de vida para cada vida restante
     for i in range(lives):
-        screen.blit(life_image,( 75 + i * 35, 50))  # Ajusta o espaçamento entre cada imagem
+        screen.blit(life_image, (75 + i * 35, 50))  # Ajusta o espaçamento entre cada imagem
 
     pygame.display.flip()  # Atualiza a tela
 
@@ -170,13 +203,17 @@ def show_sequence():
     for step in sequence:
         draw_game_elements(step)
         sound_file = f"assets/sounds/{step.lower()}.wav"
-        mixer.music.load(sound_file)
-        mixer.music.play()
+        
+        # Usando mixer.Sound para tocar o som do botão sem interromper a música de fundo
+        sound = pygame.mixer.Sound(sound_file)
+        sound.play()
+        
         pygame.time.delay(step_duration)
         draw_game_elements()  # Para limpar a tela
         pygame.time.delay(200)
     showing_sequence = False  # Permite a entrada do usuário após a sequência
     reset_timer1()  # Reinicia o timer com base na nova sequência
+
 
 # Função para verificar a sequência do usuário
 def check_sequence():
@@ -184,10 +221,11 @@ def check_sequence():
     for i in range(len(user_sequence)):
         if user_sequence[i] != sequence[i]:
             lives -= 1  # Perde uma vida ao errar a sequência
-            mixer.music.load("assets/sounds/error.wav")
-            mixer.music.play()
+            sound_error = mixer.Sound("assets/sounds/error.wav")  # Carrega o som de erro
+            sound_error.play() 
             if lives <= 0:
                 game_over = True  # Termina o jogo se as vidas acabarem
+                # Reproduz o som de erro sem interromper a música de fundo
                 return False
             else:
                 return True 
@@ -209,14 +247,118 @@ def start_game_unlimited():
     game_over = False
     lives = menu_lives  # Usa o valor de vidas ajustado no menu
     sequence.clear()
-
+    play_theme_music() 
     countdown_timer(3)
     add_step()
-    play_theme_music()  # Inicia a música tema ao começar o jogo
-    game_loop_unlimited()
+    game_loop_unlimited()  # Reinicia o loop do jogo # Reinicia o loop do jogo
+
+# Função para mostrar a tela de pause
+def display_pause_screen():
+    # Carregar a imagem de pausa
+    pause_image = pygame.image.load('assets/pause_image.png')
+    
+    # Redimensionar a imagem, se necessário (exemplo: 800x600)
+    pause_image = pygame.transform.scale(pause_image, (WIDTH, HEIGHT))
+    
+    # Exibir a imagem na tela
+    screen.blit(pause_image, (0, 0))
+    
+    # Atualizar a tela para refletir as mudanças
+    pygame.display.flip()
+
+def game_loop_unlimited():
+    global user_sequence, score, game_over, highlighted_button, ranking, time_remaining, first_game, Fase_unlimited, game_paused  # Inicializa a fase
+    if first_game:
+        reset_timer()
+    else:
+        time_remaining = time_limit + 3
+    pygame.time.set_timer(timer_event, 1000)  # Inicia o timer
+    time_sum = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:  # Pressione P para pausar o jogo
+                    game_paused = not game_paused  # Alterna o estado de pausa
+                if event.key == pygame.K_r:  # Pressione R para reiniciar
+                    restore_colors()
+                    start_game_unlimited()
+
+            if not game_paused:  # Jogo rodando
+                if not game_over:
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            sound_up = mixer.Sound("assets/sounds/up.wav")  # Carrega o efeito sonoro para UP
+                            sound_up.play()  # Reproduz o som sem interromper a música de fundo
+                            user_sequence.append('UP')
+                            highlight_button('UP')
+                            time_sum += time_remaining
+                        elif event.key == pygame.K_DOWN:
+                            sound_down = mixer.Sound("assets/sounds/down.wav")  # Carrega o efeito sonoro para DOWN
+                            sound_down.play()  # Reproduz o som sem interromper a música de fundo
+                            user_sequence.append('DOWN')
+                            highlight_button('DOWN')
+                            time_sum += time_remaining
+                        elif event.key == pygame.K_LEFT:
+                            sound_left = mixer.Sound("assets/sounds/left.wav")  # Carrega o efeito sonoro para LEFT
+                            sound_left.play()  # Reproduz o som sem interromper a música de fundo
+                            user_sequence.append('LEFT')
+                            highlight_button('LEFT')
+                            time_sum += time_remaining
+                        elif event.key == pygame.K_RIGHT:
+                            sound_right = mixer.Sound("assets/sounds/right.wav")  # Carrega o efeito sonoro para RIGHT
+                            sound_right.play()  # Reproduz o som sem interromper a música de fundo
+                            user_sequence.append('RIGHT')
+                            highlight_button('RIGHT')
+                            time_sum += time_remaining
+
+                        reset_timer()  # Reinicia o tempo ao pressionar um botão
+                        result = check_sequence()
+                        if len(sequence) >= 5 * Fase_unlimited:
+                            Fase_unlimited += 1
+                        if result is True:
+                            dim_screen()
+                            pygame.time.delay(500)
+                            score += 1 + int((time_sum * len(sequence)) / (time_limit * lives)) * Fase_unlimited
+                            user_sequence = []
+                            add_step()
+                            time_sum = 0
+                        elif result is False:
+                            game_over = True
+                            update_ranking(score, Fase_unlimited)
+
+                    # Aqui a contagem do tempo só acontece se não estivermos mostrando a sequência
+                    if event.type == timer_event and not showing_sequence:
+                        time_remaining -= 1
+                        if time_remaining <= 0:
+                            game_over = True
+
+                # Desenha os botões e o timer
+                draw_game_elements(highlighted_button)
+
+                if game_over:
+                    first_game = False
+                    display_game_over()
+                    pygame.time.wait(2000)
+                    show_menu()
+
+                pygame.time.delay(50)
+
+            else:  # Jogo pausado
+                display_pause_screen()  # Exibe a tela de pausa
+                pygame.time.delay(50)
 
 
-# Função para exibir a contagem regressiva
+
+# Função para atualizar o ranking
+def update_ranking(new_score, fase_unlimited):
+    global ranking
+    ranking.append((new_score, player_name, fase_unlimited))
+    ranking = sorted(ranking, key=lambda x: x[0], reverse=True)[:10]  # Mantém os 10 melhores
+    save_ranking()  # Salva o ranking atualizado
 def countdown_timer(seconds):
     for i in range(seconds, 0, -1):
         screen.fill(BACKGROUND_COLOR)  # Limpa a tela
@@ -228,101 +370,16 @@ def countdown_timer(seconds):
         screen.blit(countdown_surface, (WIDTH // 3, HEIGHT //3))  # Centraliza o texto
         pygame.display.flip()  # Atualiza a tela
         pygame.time.delay(1000)
-        mixer.music.load("assets/sounds/menu-selected.wav")
-        mixer.music.play()
+        sound = pygame.mixer.Sound("assets/sounds/menu-selected.wav")
+        sound.play()
         reset_timer1()
     reset_timer()
-    
-
-# Função para acender o botão pressionado
 def highlight_button(direction):
     global highlighted_button
     highlighted_button = direction
     draw_game_elements(highlighted_button)  # Desenha o botão destacado
     pygame.time.delay(200)  # Delay para mostrar o destaque
-    reset_highlight()  # Limpa o destaque após o delay
-
-# Loop principal do jogo
-def game_loop_unlimited():
-    global user_sequence, score, game_over, highlighted_button, ranking, time_remaining, first_game, Fase_unlimited
-    Fase_unlimited = 1 
-    # Inicializa o tempo restante
-    if first_game:
-        reset_timer()
-    else:
-        time_remaining = time_limit+3
-    pygame.time.set_timer(timer_event, 1000)  # Inicia o timer
-    time_sum = 0
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit_game()
-
-            if not game_over:  # Adicione essa verificação aqui
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        mixer.music.load("assets/sounds/up.wav")
-                        mixer.music.play()
-                        user_sequence.append('UP')
-                        highlight_button('UP')
-                        time_sum += time_remaining
-                    elif event.key == pygame.K_DOWN:
-                        mixer.music.load("assets/sounds/down.wav")
-                        mixer.music.play()
-                        user_sequence.append('DOWN')
-                        highlight_button('DOWN')
-                        time_sum += time_remaining
-                    elif event.key == pygame.K_LEFT:
-                        mixer.music.load("assets/sounds/left.wav")
-                        mixer.music.play()
-                        user_sequence.append('LEFT')
-                        highlight_button('LEFT')
-                        time_sum += time_remaining
-                    elif event.key == pygame.K_RIGHT:
-                        mixer.music.load("assets/sounds/right.wav")
-                        mixer.music.play()
-                        user_sequence.append('RIGHT')
-                        highlight_button('RIGHT')
-                        time_sum += time_remaining
-
-                    reset_timer()  # Reinicia o tempo ao pressionar um botão
-                    result = check_sequence()
-                    if len(sequence)>=5*Fase_unlimited:
-                        Fase_unlimited+=1
-                    if result is True:
-                        dim_screen()
-                        pygame.time.delay(500)
-                        score += 1 + int((time_sum * len(sequence))/(time_limit*lives))*Fase_unlimited
-                        user_sequence = []
-                        add_step()
-                        time_sum = 0
-                    elif result is False:
-                        game_over = True
-                        update_ranking(score, Fase_unlimited)
-
-                if event.type == timer_event and not showing_sequence:
-                    time_remaining -= 1
-                    if time_remaining <= 0:
-                        game_over = True
-
-        # Desenha os botões e o timer
-        draw_game_elements(highlighted_button)
-
-        if game_over:
-            first_game=False
-            display_game_over()
-            pygame.time.wait(2000)
-            show_menu()
-            
-        pygame.time.delay(50)
-
-# Função para atualizar o ranking
-def update_ranking(new_score, fase_unlimited):
-    global ranking
-    ranking.append((new_score, player_name, fase_unlimited))
-    ranking = sorted(ranking, key=lambda x: x[0], reverse=True)[:10]  # Mantém os 10 melhores
-    save_ranking()  # Salva o ranking atualizado
-    
+    reset_highlight() 
 def difficulty_menu():
     # Criação do submenu de dificuldade
     settings = pygame_menu.Menu('Configurações de Dificuldade', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
@@ -340,6 +397,9 @@ def difficulty_menu():
     settings.mainloop(screen)
 # Função para exibir a tela de Game Over
 def display_game_over():
+    # Restaura as cores para o estado original após o fim do jogo
+    restore_colors()
+
     # Exibe a imagem de fundo
     screen.fill((255, 0, 0))
     tela = pygame.image.load("assets/Tela.png")
@@ -347,8 +407,8 @@ def display_game_over():
     screen.blit(tela_image, (0, 0))
     
     # Toca o som de erro
-    #mixer.music.load("assets/sounds/error.wav")
-    #mixer.music.play()
+    mixer.music.load("assets/sounds/error.wav")
+    mixer.music.play()
     
     # Configura a fonte e renderiza o texto de "Game Over" e "Score"
     font = pygame.font.Font(None, 74)
@@ -400,6 +460,7 @@ def settings_menu():
     
     # Entrada de texto para o nome do jogador
     settings.add.text_input('Nome do Jogador: ', default=player_name, onchange=set_player_name)
+    
     # Slider para ajustar o tempo do timer em segundos
     settings.add.range_slider('Tempo do Timer (segundos):', 
                               default=time_limit, 
@@ -421,6 +482,13 @@ def settings_menu():
                               increment=1,
                               onchange=lambda value: set_lives(value))  # Atualiza o número de vidas
     
+    # Slider para ajustar a fase atual
+    settings.add.range_slider('Fase Atual:', 
+                              default=Fase_unlimited, 
+                              range_values=(1, 4),  # Defina um valor máximo adequado para a fase
+                              increment=1,
+                              onchange=lambda value: set_fase(value))  # Atualiza a fase
+    
     # Botão para voltar ao menu principal
     settings.add.button('Voltar', show_menu)
     
@@ -429,7 +497,7 @@ def settings_menu():
         events = pygame.event.get()  # Obtém os eventos do Pygame
         settings.update(events)  # Atualiza o menu com os eventos
         settings.draw(screen)  # Desenha o menu na tela
-        pygame.display.flip()    # Atualiza a tela
+        pygame.display.flip()     # Atualiza a tela
 
 def set_lives(value):
     global lives, menu_lives
@@ -438,7 +506,7 @@ def set_lives(value):
 # Função para definir o tempo do timer
 def show_menu():
     screen.blit(background_image, (0, 0))  # Aplica a imagem de fundo no menu principal
-    menu = pygame_menu.Menu('Genius Game', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
+    menu = pygame_menu.Menu('Jeremias, The Genius', WIDTH, HEIGHT, theme=pygame_menu.themes.THEME_DARK)
 
     menu.add.button('Começar Jogo', start_game_unlimited)  # Opção para iniciar o jogo
     menu.add.button('Dificuldade', difficulty_menu)  # Opção para o menu de dificuldades
